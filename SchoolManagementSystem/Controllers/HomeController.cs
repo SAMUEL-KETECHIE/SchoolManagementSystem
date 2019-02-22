5318a048a342d4334935998e93c8ea315d07bf2a
SAMUELWENDOLIN KETECHIE <samuelwendolin2@live.com>
2019-02-22 05:31:14 +0000

    First UI design

---
+++ SchoolManagementSystem/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SchoolManagementSystem.Models;
+
+namespace SchoolManagementSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["user"] = "Admin";
+            if (ViewData["user"].ToString() != "Admin")
+                return View();
+            return View(Admin());
+        }
+
+        public IActionResult Subjects()
+        {
+            return View();
+        }
+
+        public IActionResult Students()
+        {
+            return View();
+        }
+
+        public IActionResult Admin()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
